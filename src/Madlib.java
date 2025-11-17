9e216e85b8fcbf4581341f94071435fc2afea6e3
GabrielMilton <gabriel_galva-diez28@milton.edu>
2025-11-17 13:48:48 -0500

    Almost done

--- src/Madlib.java
+++ src/Madlib.java
@@ -1,5 +1,16 @@
 public class Madlib {
     public static void main(String[] args) {
-        
+        System.out.println("Hello world");
+        Madlib mystory = new Madlib();
+
+    }
+
+    public Madlib() {
+        String classmate = "Niam";
+        String city = "George Town";
+        String noun = "teleportaion device";
+        String adjective = "velmputous";
+
+
     }
 }
